a683361804645d327c934d83720ee845183884e7
B3N1N1 <122571115+B3N1N1@users.noreply.github.com>
2023-04-20 02:21:24 -0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,15 @@
+module hallGO
+
+go 1.19
+
+require github.com/fatih/color v1.15.0
+
+require (
+	github.com/Ullaakut/nmap v2.0.2+incompatible // indirect
+	github.com/Ullaakut/nmap/v2 v2.2.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+    "bufio"
+	"os"
+    "net/http"
+	"os/exec"
+	"github.com/fatih/color"
+    "net"
+    "strconv"
+    "sync"
+    "time"
 )
 
+
+
 func main() {
+	c := color.New(color.FgRed)
+	c.Add(color.FgYellow)
+	c.Add(color.FgGreen)
+
     var opcao int
 
     for {
-        fmt.Println("MENU:")
-        fmt.Println("1. Opção 1")
-        fmt.Println("2. Opção 2")
-        fmt.Println("3. Opção 3")
-        fmt.Println("0. Sair")
-        fmt.Print("Escolha uma opção: ")
+		clear()
+        c.Println("MENU:")
+        c.Println("1. DIRBUSTER")
+        c.Println("2. PORTSCAN")
+        c.Println("3. Opção 3")
+        c.Println("0. Sair")
+        c.Print("Escolha uma opção: ")
         fmt.Scanln(&opcao)
-
+		
         switch opcao {
         case 0:
             fmt.Println("Saindo do programa...")
             return
         case 1:
-            direc()
+            dirgo()
         case 2:
-            fmt.Println("Você escolheu a opção 2")
+            port()
         case 3:
             fmt.Println("Você escolheu a opção 3")
         default:
@@ -31,30 +47,70 @@ func main() {
         }
     }
 }
-func direc() {
-    var opcao int
+func clear() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+func dirgo() {
+    scanner := bufio.NewScanner(os.Stdin)
 
-    for {
-        fmt.Println("MENU:")
-        fmt.Println("1. scan")
-        fmt.Println("2. Opção 2")
-        fmt.Println("3. Opção 3")
-        fmt.Println("0. Sair")
-        fmt.Print("Escolha uma opção: ")
-        fmt.Scanln(&opcao)
+    fmt.Print("Digite a URL alvo: ")
+    scanner.Scan()
+    targetURL := scanner.Text()
 
-        switch opcao {
-        case 0:
-            fmt.Println("Saindo do programa...")
-            return
-        case 1:
-            fmt.Println("Você escolheu a opção 1")
-        case 2:
-            fmt.Println("Você escolheu a opção 2")
-        case 3:
-            fmt.Println("Você escolheu a opção 3")
-        default:
-            fmt.Println("Opção inválida. Tente novamente.")
+    fmt.Print("Digite o caminho da wordlist: ")
+    scanner.Scan()
+    wordlistPath := scanner.Text()
+
+    wordlistFile, err := os.Open(wordlistPath)
+    if err != nil {
+        fmt.Println("Erro ao abrir o arquivo:", err)
+        return
+    }
+    defer wordlistFile.Close()
+
+    scanner = bufio.NewScanner(wordlistFile)
+
+    for scanner.Scan() {
+        dir := scanner.Text()
+
+        url := targetURL + "/" + dir
+        resp, err := http.Get(url)
+
+        if err != nil {
+            fmt.Println("Erro ao acessar o diretório", dir, err)
+        } else if resp.StatusCode == 200 {
+            fmt.Println("Diretório encontrado:", dir)
         }
     }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Println("Erro ao ler o arquivo:", err)
+        return
+    }
+}
+func port() {
+    var target string
+    fmt.Print("Digite o endereço IP do alvo: ")
+    fmt.Scanln(&target)
+
+    var wg sync.WaitGroup
+    for port := 1; port <= 65535; port++ {
+        wg.Add(1)
+        go func(port int) {
+            defer wg.Done()
+            address := target + ":" + strconv.Itoa(port)
+            conn, err := net.DialTimeout("tcp", address, 100*time.Millisecond)
+
+            if err != nil {
+                return
+            }
+
+            conn.Close()
+            fmt.Printf("Porta %d aberta\n", port)
+        }(port)
+    }
+
+    wg.Wait()
 }
